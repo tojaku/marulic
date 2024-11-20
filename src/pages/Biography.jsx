e6f57f949e58584ebcb1469e15f0305d8102bb26
Matija <matija.baresic@skole.hr>
2024-11-20 11:57:49 +0100

    biografija i stvaralastvo

--- src/pages/Biography.jsx
+++ src/pages/Biography.jsx
@@ -1,5 +1,54 @@
 export default function Biography(props) {
     return (
-        <div>Biografija</div>
+        <div><h1 class="text-3xl font-bold text-white">Biografija</h1>
+            <div>
+                <br />
+                Marko Marulić (lat. <i>Marcus Marulus Spalatensis</i>), rođen 18. kolovoza 1450. u Splitu, 
+                preminuo 5. siječnja 1524. u istome gradu, bio je hrvatski književnik, kršćanski humanist
+                 i otac hrvatske književnosti. Često se spominje s etnikom "Splićanin". Njegov bogati i 
+                 raznovrsni književni opus ističe se iznimnom stilizacijom, znanjem i prilagodljivošću 
+                 raznovrsnim čitateljskim publikama.
+                <div>Djela i jezici</div>
+                Marulić je napisao više od 40 djela na hrvatskom, latinskom i talijanskom jeziku, koja 
+                su tiskana više od 530 puta kroz povijest. Njegova djela prevedena su na brojne jezike, 
+                uključujući engleski, francuski, njemački, japanski i španjolski. Premda je većinu 
+                djela napisao na latinskom jeziku, ostavio je važan doprinos i na hrvatskom jeziku, 
+                čime je značajno utjecao na razvoj hrvatske književnosti.
+                <div>Književni utjecaji</div>
+                Inspiraciju za svoja djela nalazio je u Bibliji, antičkoj književnosti i patrističkim 
+                tekstovima. U latinskim djelima potpisivao se kao Marcus Marulus Spalatensis ili 
+                Marcus Marulus Delmata, dok je na hrvatskome koristio ime Marko Pečenić. Zanimljivo, 
+                prva zabilježena uporaba riječi "psihologija" pripisuje se upravo njemu, u djelu Psichiologia de ratione animae humanae.
+                <div>Utjecaj na suvremenike</div>
+                Veliki domet Marulićeva stvaralaštva potvrđuju i anegdote iz njegova doba. Tako je 
+                Thomas More čitao njegov Evanđelistar iz 1516. dok je bio zatočen prije pogubljenja, 
+                a sv. Franjo Saleški uvijek je sa sobom nosio Institucije (1506).
+                <div>Svetost i počast</div>
+                U splitskoj crkvenoj zajednici spominjan je kao "sluga Božji" jer je umro na glasu 
+                svetosti, iako službeni proces beatifikacije nije pokrenut. Godine 1996. Hrvatski 
+                sabor proglasio je 22. travnja Danom hrvatske knjige u spomen na dovršetak Judite 
+                1501. godine. Pored toga, 2021. i 2024. obilježene su kao Godine Marka Marulića.
+                <div>Obitelj i obrazovanje</div>
+                Marulić je rođen u plemićkoj obitelji Marulić, koja se od 1436. nazivala i Pečenić. 
+                Roditelji, Nikola i Dobrica Obirtić (Alberti), pripadali su uglednim plemićkim obiteljima. 
+                Marulić je bio najstariji od osmero djece. Obrazovanje je stekao u Splitu i, prema pretpostavkama, 
+                studirao u Padovi. Profesionalno se bavio pravom, obnašajući razne gradske dužnosti, poput suca i odvjetnika.
+                <div>Književna karijera</div>
+                Prvo poznato Marulićevo djelo, Evanđelistar (1480.), rasprava o pobožnom životu, tiskano je u 
+                Mlecima i tijekom dva desetljeća doživjelo je 60 izdanja. Njegovo najpoznatije djelo na hrvatskom 
+                jeziku, spjev Judita, dovršeno je 1501. godine. Osim toga, na latinskom jeziku napisao je niz 
+                teoloških i moralističkih djela, poput De institutione bene vivendi (1506.) i De humilitate et gloria Christi (1519.).
+                <div>Povlačenje na Šoltu</div>
+                U zreloj dobi povukao se u samotnu kućicu uz crkvicu sv. Petra na Šolti, gdje je provodio vrijeme u 
+                molitvi i književnom radu. Ondje je preveo Petrarcinu kanconu Vergine bella i napisao brojna druga djela. 
+                Putovao je u Veneciju i Rim te ostao aktivan u društvenim i vjerskim pitanjima.
+                <div>Nasljeđe i smrt</div>
+                Marulić je preminuo 1524. u Splitu i pokopan je u crkvi sv. Frane. Njegov značaj kao oca hrvatske 
+                književnosti očituje se u bogatom književnom opusu, nacionalnom ponosu i humanističkim vrijednostima 
+                koje su nadilazile granice Hrvatske.
+            </div>
+        </div>
+
+
     );
 }
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,5 +1,72 @@
 export default function Works(props) {
     return (
-        <div>Stvaralaštvo</div>
+        <div>Stvaralaštvo
+            <div>
+                Marko Marulić bio je kršćanski humanist i renesansni književnik koji je svoj opus temeljio na harmoniji klasične antičke mudrosti, kršćanske etike i srednjovjekovne pobožnosti. Njegova djela svjedoče o visokom obrazovanju i nadahnuću iz Biblije, hagiografije, patristike, antičke književnosti te suvremenih povijesnih zbivanja. Pisao je na latinskom, hrvatskom i talijanskom jeziku, pri čemu 80% opusa čini latinska književnost. Njegov književni stil kombinira čakavštinu, staroslavizme, štokavske elemente te utjecaje splitske renesansne kulture. Prepoznat je kao zagovornik kršćanskog morala i etike, čije su misli ostavile značajan utjecaj na europsku književnost.
+
+                <div>Djela i poznati citati</div>
+                <div class="pl-10">
+                <div>Hrvatska djela</div>
+                <div>1. Judita (1501., prvo izdanje 1521.)</div>
+                ◦ Opis: Prvi umjetnički ep hrvatske književnosti napisan na čakavštini. Temeljen je na biblijskoj priči o udovici Juditi koja spašava izraelski narod. Inspiriran je borbom protiv Osmanlija te odražava kršćanski moral i patriotske poruke. <br />
+                ◦ Citat: „Ter ča mogu začut Turci svi prosti, / da te plaća triba / vrhušac i dosti?“<br />
+                ◦ Značaj: Djelo je snažan simbol otpora i nacionalnog identiteta. <br />
+                <div>2. Suzana</div>
+                ◦ Opis: Biblijska poema u 780 stihova o babilonskoj Židovki lažno optuženoj za preljub. U ovom djelu Marulić spominje „hrvatske knjige“, čime ukazuje na svoju književnu baštinu.
+                ◦ Citat: „Bog vidit prosti, / on spravit zlosti!“
+                <div>3. Molitva suprotiva Turkom</div>
+                ◦ Opis: Pjesma od 172 dvostruko rimovana dvanaesterca, s akrostihom koji glasi: Solus Deus potes nos liberare de tribulatione inimicorum Turcorum sua potentia infinita.
+                ◦ Citat: „Pomiluj nas, gospodine, priko svela / od pogibla i od zla vrimega!“
+                <div>4. Tuženje grada Hjerosolima</div>
+                ◦ Opis: Lamentacija protuturske tematike o osmanlijskim napadima.
+                ◦ Citat: „Gospodi moja, narode, gdi ste? / Jer ne branite vrimega zlosta?“
+                <div>5. Dobri nauci</div>
+                ◦ Opis: Pjesme nabožne tematike koje ističu važnost pobožnosti i kršćanskih vrjednota.
+                ◦ Citat: „Ne ufaj se u blaga ovoga svita, / već u dušu tvoju pravu gospita.“
+                <div>6. Čten’je života i muke blaženoga i sfetoga Dujma</div>
+                ◦ Opis: Životopis splitskog zaštitnika sv. Dujma, prožet dubokom kršćanskom duhovnošću. </div> <br />
+
+                <div class="pl-10">
+                <div>Latinska djela</div>
+                <div>1. Davidias (oko 1517.)</div>
+                ◦ Opis: Ep u 14 knjiga i 6765 heksametara o kralju Davidu, pisan po uzoru na Vergilija. Djelo je alegorijsko – David predstavlja Krista, a Šaul Židove.
+                ◦ Citat: „Domine, dirige me ut non labar adversum minas inimicorum meorum.“
+                ◦ Značaj: Smatra se vrhuncem europskog humanizma.
+                <div>2. De institutione bene vivendi per exempla sanctorum</div>
+                ◦ Opis: Zbirka poučnih priča iz Biblije i života svetaca, jedno od najprevođenijih Marulićevih djela.
+                ◦ Citat: „Veritas nos semper ad lucem ducet.“
+                ◦ Značaj: Prevedeno na desetke jezika, uključujući japanski.
+                <div>3. Evangelistarium</div>
+                ◦ Opis: Moralistička rasprava u sedam knjiga o praktičnoj kršćanskoj etici.
+                ◦ Citat: „Amor Dei principium omnis virtutis est.“
+                <div>4. De humilitate et gloria Christi</div>
+                ◦ Opis: Kristološka rasprava o poniznosti i slavi Krista, usmjerena na dokazivanje Kristova mesijanstva.
+                ◦ Citat: „Christus humilitatem docuit, ut gloriam nobis aperiret.“
+                <div>5. Dialogus de Hercule a Christocolis superato</div>
+                ◦ Opis: Filozofska rasprava o odnosu mitologije i kršćanstva.
+                ◦ Citat: „Non victoriae humanae sed Christi triumphus superavit omnia.“
+                <div>6. De ultimo Christi iudicio</div>
+                ◦ Opis: Retorički najsavršeniji tekst o Posljednjem sudu Kristovu.
+                ◦ Citat: „Justitia eius lucem adducet die novissima.“
+                <div>7. Vita divi Hieronimi</div>
+                ◦ Opis: Biografija sv. Jeronima, s polemičkom raspravom protiv onih koji su tvrdili da je bio Talijan.
+                ◦ Citat: „Hieronimus, lux Ecclesiae et patronus linguae sanctae.“
+                <div>8. Epigrammata</div>
+                ◦ Opis: Ciklus od 141 epigrama, inspiriran Katulom i Ovidijem.
+                ◦ Citat: „Non omnis est amor purus; tamen in puritate spes nostra manet.“
+                <div>9. Quinquaginta parabolae</div>
+                ◦ Opis: Zbirka pedeset prispodoba, posvećena Tomi Nigeru, s tematikom inspiriranom Biblijom.
+                ◦ Citat: „Parabola semper in lucem ducit Veritatem.“
+                <div>10. Regum Dalmatiae et Croatiae gesta</div>
+                • Opis: Slobodan prijevod dijela Ljetopisa Popa Dukljanina, poznat i kao Hrvatska kronika.
+                • Citat: „Historia servanda est memoriae gentis nostrae.“</div>
+
+                Zaključak
+                Marulićeva djela odražavaju njegovu intelektualnu širinu i duboko ukorijenjenu duhovnost. Bilo da piše o biblijskim junacima,
+                kršćanskoj etici ili aktualnim političkim temama, njegovi tekstovi nose poruke univerzalne vrijednosti i važnosti moralne dosljednosti.
+                Značajna ostavština, kako na nacionalnoj, tako i na europskoj razini, čini ga nezaobilaznim autorom renesansne književnosti.
+            </div>
+
+        </div>
     );
 }
