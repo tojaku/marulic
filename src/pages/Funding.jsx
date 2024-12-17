902d17e58afe3954533324dcc815efae884314ea
valentinfuntak <valentinfuntakk@gmail.com>
2024-12-17 18:02:35 +0100

    Ispravak gramatike

--- src/pages/Funding.jsx
+++ src/pages/Funding.jsx
@@ -6,25 +6,37 @@ export default function Funding(props) {
             </div>
             <div class="text-lg leading-relaxed">
                 <p class="mb-4">
-                    U sklopu školskog projekta, učenici različitih strukovnih područja elektrotehnike, kulinarstva, računalstva i drugih okupili su se kako bi istražili i prikazali povezanost svojih zanimanja s Markom Marulićem, <i>Ocem hrvatske književnosti</i>. Cilj projekta bio je potaknuti interdisciplinarni pristup učenju te oživjeti Marulićevo stvaralaštvo na inovativan način.
+                U sklopu školskog projekta <i>Marko Marulić u OBŠKC </i> učenici različitih strukovnih područja okupili su se kako bi istražili i prikazali povezanost svojih zanimanja s djelom i životom Marka Marulića, Oca hrvatske književnosti. Cilj projekta bio je potaknuti interdisciplinarni pristup učenju te oživjeti Marulićevo stvaralaštvo na inovativan način.
                 </p>
                 <h2 class="text-xl font-semibold mb-3">Primjeri aktivnosti po zanimanjima:</h2>
                 <ul class="list-disc list-inside mb-4">
                     <li>
-                        <strong>Elektrotehnika:</strong> Učenici su osmislili modernu prezentaciju “Judita” pomoću interaktivne svjetlosne instalacije, simbolizirajući Marulićeve svjetlosne poruke moralnosti i duhovnosti.
+                        <strong>Elektrotehnika:</strong> Učenici su osmislili modernu prezentaciju <i>Judita</i> pomoću interaktivne svjetlosne instalacije, simbolizirajući Marulićeve svjetlosne poruke moralnosti i duhovnosti. Od elektroničkih komponenata izrađene su skulpture koje prikazuju Marka Marulić i Juditu.
                     </li>
                     <li>
-                        <strong>Kulinarstvo:</strong> Inspirirani vremenom Marka Marulića, učenici su istraživali recepte iz doba renesanse i pripremali jela karakteristična za Split u 15. i 16. stoljeću.
+                        <strong>Kuhar, slastičar, pekar, mesar, konobar:</strong> Inspirirani vremenom Marka Marulića, učenici su istraživali recepte iz doba renesanse, oblikovali e-kuharicu te pripremali i poslužili jela karakteristična za Split 15. i 16. stoljeća.
                     </li>
                     <li>
                         <strong>Računalstvo:</strong> Napravljen je multimedijalni projekt, uključujući animacije i aplikaciju koja korisnicima omogućuje interaktivno istraživanje života i djela Marka Marulića.
                     </li>
                     <li>
-                        <strong>Umjetnost i dizajn:</strong> Učenici su dizajnirali plakate i digitalne ilustracije koje spajaju motive iz “Judita” s modernim vizualnim izričajem.
+                        <strong>Strojarstvo:</strong> Učenici su izradili portret Marka Marulića u limu. 
+                    </li>
+                    <li>
+                        <strong>Stolar:</strong> Učenici su izradili okvir za portret Marka Marulića.
+                    </li>
+                    <li>
+                        <strong>Soboslikar-ličilac:</strong> Obojan je zid na kojemu je izložen portret Marka Maruliča te su ispisani stihovi s početka <i>Judite</i>.
+                    </li>
+                    <li>
+                        <strong>Umjetnost i dizajn:</strong> Učenici su dizajnirali plakate i digitalne ilustracije koje spajaju motive iz <i>Judite</i> s modernim vizualnim izričajem. Oblikovali su e-knjigu priča nastalih nakon čitanja Marulićeve <i>Judite</i>.
                     </li>
                 </ul>
                 <p>
-                    Ovaj projekt omogućio je učenicima ne samo da prodube svoje razumijevanje Marulićeva rada, već i da razvijaju kreativne vještine, surađuju međusobno i predstave bogatstvo hrvatske kulturne baštine na suvremen način.
+                Ovaj projekt omogućio je učenicima ne samo da prodube svoje razumijevanje Marulićeva rada, već i da razvijaju kreativne vještine, surađuju međusobno i predstave bogatstvo hrvatske kulturne baštine na suvremen način.
+                </p>
+                <p class="mt-4">
+                Učenike su u aktivnostima pratili profesori: Melita Golubić, Vesna Rončević-Perić, Saša Vorkapić Oraškić, Petra Sočev Aščić, Danijela Vrtiprah, Danijel Lovrenčić, Tomislav Jakupić, Slavko Franjo, Igor Sraček, Andrijana Manojlović, Blaženka Pianec, Grozdana Bohorč, Ksenija, Sedlanić, Damir Đelekovčan, Krunoslav Kukec.
                 </p>
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ export default function Home(props) {
                 </div>
                 <div class="lg:w-1/2 text-lg">
                     <p>
-                        Kako bi obilježila 500. obljetnicu smrti Marka Marulića, Vlada Republike Hrvatske proglasila je 2024. godinu Godinom Marka Marulića.Ako ovo čitate, znači da ste skenirali jedan od QR kodova s plakata i stigli na ovu stranicu. Stranica omogućuje uvid u biografiju Marka Marulića, njegovo stvaralaštvo te donosi zanimljive radove učenika OBŠKC koji su nastali nakon čitanja Marulićeva djela. Ova je stranica dio projekta Marulić u OBŠKC kojim su učenici naše škole obilježili Godinu posvećenu Ocu hrvatske književnost.
+                        Kako bi obilježila 500. obljetnicu smrti Marka Marulića, Vlada Republike Hrvatske proglasila je 2024. godinu Godinom Marka Marulića. Ako ovo čitate, znači da ste skenirali jedan od QR kodova s plakata i stigli na ovu stranicu. Stranica omogućuje uvid u biografiju Marka Marulića, njegovo stvaralaštvo te donosi zanimljive radove učenika OBŠKC koji su nastali nakon čitanja Marulićeva djela. Ova je stranica dio projekta Marulić u OBŠKC kojim su učenici naše škole obilježili Godinu posvećenu Ocu hrvatske književnost.
                     </p>
                     <p class="mt-4 font-semibold">Uživajte!</p>
                 </div>
